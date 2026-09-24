65340241ad56608de2cecf8688c78197b3e0fdea
Reav´ <maxball591@gmx.de>
2026-09-24 10:43:45 +0200

    Supabase verbinden und Hero-Motiv bereitstellen

--- config.js
+++ config.js
@@ -1,6 +1,5 @@
-// Only public values belong here. Never use a service_role or secret key.
 window.HEART_CONFIG = {
-  supabaseUrl: '',
-  supabaseAnonKey: '',
+  supabaseUrl: 'https://tapeuwpasmwpltysczur.supabase.co',
+  supabaseAnonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRhcGV1d3Bhc213cGx0eXNjenVyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3OTAyMTAyMTQsImV4cCI6MjEwNTc4NjIxNH0.52-7N74gCfr33tmF6CGruj-ifNi5Dua6EQVcmBjVklk',
   publicUrl: 'https://reaveexx.github.io/herzfrequenz/',
 };
